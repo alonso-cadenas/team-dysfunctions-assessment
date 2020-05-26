211143c526b2f19a2ff81d97857115140de5fc06
Alonso Cadenas <alonso.cadenas@gartner.com>
2020-05-26 18:42:34 -0500

    Properly initialize and clone answers array.

--- components/assessment.js
+++ components/assessment.js
@@ -2,10 +2,10 @@ import React, {useState} from 'react';
 import {Frequency, questions} from '../constants';
 
 export function Assessment() {
-    const [answers, setAnswers] = useState([]);
+    const [answers, setAnswers] = useState(new Array(questions.length));
     const updateAnswer = (event) => {
         const index = event.target.name.replace('answer', '');
-        const newAnswers = answers;
+        const newAnswers = [...answers];
         newAnswers[index] = event.target.value;
         setAnswers(newAnswers);
     };
