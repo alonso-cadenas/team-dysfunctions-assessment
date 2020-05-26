8c76c0f42a612c707090c7c11f78a3fa0bb04b4f
Alonso Cadenas <alonso.cadenas@gartner.com>
2020-05-25 23:53:31 -0500

    Implement static questionnaire.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Assessment from '../components/assessment';
+import React from 'react';
+
 export default function Home() {
-  return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Five Dysfunctions of a Team Online Assessment</h1>
+
+            <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
+                <a>About</a>
+            </Link>
+
+            <Assessment/>
+        </div>
+    )
 }
