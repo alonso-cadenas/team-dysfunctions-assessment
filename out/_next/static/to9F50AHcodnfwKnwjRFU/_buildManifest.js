b805a38d49158b52d849432944cf23c868475905
Alonso Cadenas <alonso.cadenas@gartner.com>
2020-06-06 13:01:26 -0500

    Deploy Next.js to gh-pages

---
+++ out/_next/static/to9F50AHcodnfwKnwjRFU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/assessment":[a],"/index":[a]}}("static\u002Fchunks\u002F78c9ce22f5d4e72fb1e1b38e77a1652439ce8235.3674c85eea9ba6ac3fe2.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
